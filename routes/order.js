5ad10fba919bbf6cf563f31f990a2bfa478e4325
kisnahc <kisnahc@naver.com>
2020-09-12 00:43:32 +0900

    route (CRUD)#2

--- routes/order.js
+++ routes/order.js
@@ -2,9 +2,26 @@ const express = require('express')
 
 const router = express.Router()
 
-
-
-
+router.post('/',(req, res)=>{
+    res.json({
+        msg: 'order create API'
+    })
+})
+router.patch('/',(req, res)=>{
+    res.json({
+        msg: 'order update API'
+    })
+})
+router.get('/', (req, res)=>{
+    res.json({
+        msg: 'order read API'
+    })
+})
+router.delete('/', (req, res)=>{
+    res.json({
+        msg: 'order delete API'
+    })
+})
 
 
 
