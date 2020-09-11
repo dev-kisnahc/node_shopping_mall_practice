4047daf5f1167434a742436f9ea4460e808ff7be
kisnahc <kisnahc@naver.com>
2020-09-11 23:35:31 +0900

    req, res testing

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express')
 
 const app = express()
 
+app.use((req, res) =>{
+     res.json({
+         msg : 'seccessful data'
+     })
+})
+
 const PORT = 5000
 
 app.listen(PORT, () => console.log('server started'))
